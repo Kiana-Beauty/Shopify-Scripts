8c91a3d4edd7b65857c08c6e4a9e0eb968b5ca7f
Peter Macinkovic <peter.mac@inkovic.com>
2017-03-15 12:59:05 +1100

    Create ads.js

---
+++ AdBlock Tracking/ads.js
@@ -0,0 +1,6 @@
+var e=document.createElement('div');
+e.id='upBwXaklLFUR';
+e.style.display='block';
+e.style.height='1px';
+document.body.appendChild(e);
+// Modified script from http://www.detectadblock.com/ . Issue with display none not registering...
